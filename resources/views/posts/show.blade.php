f51dc891e531885aec1ec55c7b626f1871fd50d1
rahulii <r.sawra@gmail.com>
2018-10-15 19:17:17 +0530

    Solved middleware and authentication part

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,9 +9,12 @@
 <small>Written on : {{$post->created_at}}</small>
 <hr>
 <div class="container">
+@if(!Auth::guest())
 <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+
 {!!Form::open(['action' => ['PostsController@destroy', $post->id],'method' =>'POST','class' =>'pull-right'])!!}   
     {{Form::hidden('_method','DELETE')}}
     {{Form::submit('Delete',['class' =>'btn btn-danger'])}}
 {!!Form::close()!!}
+@endif
 @endsection

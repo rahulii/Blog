89b695f16a51adaa312bcecf4b55d5dcbe139f17
rahulii <r.sawra@gmail.com>
2018-10-15 19:23:34 +0530

    futher improvements in Auth

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,11 +10,13 @@
 <hr>
 <div class="container">
 @if(!Auth::guest())
+@if(Auth::user()->id == $post->user_id)
 <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
 
 {!!Form::open(['action' => ['PostsController@destroy', $post->id],'method' =>'POST','class' =>'pull-right'])!!}   
     {{Form::hidden('_method','DELETE')}}
     {{Form::submit('Delete',['class' =>'btn btn-danger'])}}
 {!!Form::close()!!}
+@endif
 @endif
 @endsection
